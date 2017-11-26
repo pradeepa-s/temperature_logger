a0b76dd09720cbc3ddbe232c494fab2afe138ac1
Pradeepa Senanayake <pradeepas@masholdings.com>
2017-11-26 11:23:15 +0530

    Added the error handler to blink red led

--- temperature_logger/Src/main.c
+++ temperature_logger/Src/main.c
@@ -308,6 +308,9 @@ void _Error_Handler(char * file, int line)
   /* User can add his own implementation to report the HAL error return state */
   while(1) 
   {
+	  HAL_Delay(250);
+
+	  HAL_GPIO_TogglePin(LD_R_GPIO_Port, LD_R_Pin);
   }
   /* USER CODE END Error_Handler_Debug */ 
 }
